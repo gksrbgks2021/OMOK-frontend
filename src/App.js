9bfeca77ae6528c1d9940abcc3705cd83d61505f
namsunghyun3057 <namsunghyen453@gmail.com>
2023-11-28 14:26:57 +0900

    confer contents

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import "./App.css";
 
 function App() {
   /*
+  //반응형 함수(?) 일단은 안씀
   function setScreenSize() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
