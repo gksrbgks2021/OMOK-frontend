6b707a2d6d4e4b8aa9b04ca3c88552e6b6892864
Kyprosa <propolis200@gmail.com>
2024-01-02 17:00:36 +0900

    ranking

--- src/pages/rank.js
+++ src/pages/rank.js
@@ -1,7 +1,30 @@
+import "../styles/rankStyle.css";
+
 function Rank() {
   return (
-    <div>
+    <div id="wrapper">
       <h2>Rank</h2>
+      <div id="block">
+        <div id="second">
+          <div className="user">user</div>
+          <div className="text">2</div>
+        </div>
+        <div id="first">
+          <div className="user">user</div>
+          <div className="text">1</div>
+        </div>
+        <div id="third">
+          <div className="user">user</div>
+          <div className="text">3</div>
+        </div>
+      </div>
+      <div id="others">
+        <li>
+          <ol className="user">user</ol>
+          <ol className="user">user</ol>
+          <ol className="user">user</ol>
+        </li>
+      </div>
     </div>
   );
 }
