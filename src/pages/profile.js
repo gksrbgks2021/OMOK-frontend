91573379d0e3feff89dfd01c0242fc37603d0711
Kyprosa <propolis200@gmail.com>
2024-01-17 16:32:36 +0900

    Revert "profile"
    This reverts commit ec55d51fa6021b4596ce98653872e4899a9a7be6.

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,40 +1,10 @@
 import User from "../components/User";
 
-function Profile(props) {
+function Profile() {
   return (
     <div>
-      <hr></hr>
-      <hr></hr>
+      <h2>Profile</h2>
       <User />
-      <hr></hr>
-      <div id="daily-streak">daily check-in streak {props.streak} 3days</div>
-      <hr></hr>
-      <div id="stats">
-        <div id="latest">
-          Latest Stats <br></br>
-          10G 6W 4L 0D (60%)
-          {props.latest}
-        </div>
-        <hr></hr>
-        <div id="overall">
-          Overall Stats <br></br>
-          10G 6W 4L 0D (60%)
-          {props.overall}
-        </div>
-        <hr></hr>
-        <div id="black">
-          Black Stats <br></br>
-          10G 6W 4L 0D (60%)
-          {props.black}
-        </div>
-        <hr></hr>
-        <div id="white">
-          White Stats <br></br>
-          10G 6W 4L 0D (60%)
-          {props.white}
-        </div>
-      </div>
-      <hr></hr>
     </div>
   );
 }
