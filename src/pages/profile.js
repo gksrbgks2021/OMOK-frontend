ec55d51fa6021b4596ce98653872e4899a9a7be6
Kyprosa <propolis200@gmail.com>
2024-01-17 15:45:34 +0900

    profile

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,10 +1,40 @@
 import User from "../components/User";
 
-function Profile() {
+function Profile(props) {
   return (
     <div>
-      <h2>Profile</h2>
+      <hr></hr>
+      <hr></hr>
       <User />
+      <hr></hr>
+      <div id="daily-streak">daily check-in streak {props.streak} 3days</div>
+      <hr></hr>
+      <div id="stats">
+        <div id="latest">
+          Latest Stats <br></br>
+          10G 6W 4L 0D (60%)
+          {props.latest}
+        </div>
+        <hr></hr>
+        <div id="overall">
+          Overall Stats <br></br>
+          10G 6W 4L 0D (60%)
+          {props.overall}
+        </div>
+        <hr></hr>
+        <div id="black">
+          Black Stats <br></br>
+          10G 6W 4L 0D (60%)
+          {props.black}
+        </div>
+        <hr></hr>
+        <div id="white">
+          White Stats <br></br>
+          10G 6W 4L 0D (60%)
+          {props.white}
+        </div>
+      </div>
+      <hr></hr>
     </div>
   );
 }
