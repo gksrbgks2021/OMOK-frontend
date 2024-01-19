d5103e4f26ce5fedb34e901e94c8c64f4203473f
Kyprosa <propolis200@gmail.com>
2024-01-19 13:58:51 +0900

    app

--- src/App.js
+++ src/App.js
@@ -11,14 +11,57 @@ import Profile from "./pages/profile.js";
 import NavigationBar from "./NavigationBar.js";
 import StatusBar from "./StatusBar.js";
 import "./App.css";
+import { GOOGLE, KAKAO, GUEST } from "./constants/VARIABLE";
+import axios from "axios";
+import { URL_POST_TEST } from "./constants/UrlConstants";
 
 function App() {
   const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수 얻기
+
   const [showMain, setShowMain] = useState(() => {
     // 로컬 스토리지에서 showMain 상태를 불러오기
     const storedShowMain = localStorage.getItem("showMain");
     return storedShowMain ? JSON.parse(storedShowMain) : true;
   });
+
+  const handleLoginClick = (category) => {
+    switch (category) {
+      case GOOGLE:
+        handleGoogleLogin();
+        break;
+      case KAKAO:
+        break;
+      case GUEST:
+        handleGuestLogin();
+        break;
+    }
+    setShowMain(!isShowMain);
+  };
+
+  const handleGoogleLogin = () => {
+    const url = URL_POST_TEST;
+    console.log(URL_POST_TEST);
+    axios({
+      method: "post",
+      url: url,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(`응답: `, response);
+        let data = response.data;
+        if (response.data) {
+          console.log("data", data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error during POST request:", error);
+      });
+  };
+  const handleGuestLogin = () => {
+    console.log("게스트 로그인 처리합니다");
+  };
 
   const handleButtonClick = () => {
     setShowMain(!showMain);
@@ -31,7 +74,7 @@ function App() {
   }, [showMain]);
 
   if (showMain) {
-    return <Main onButtonClick={handleButtonClick} />;
+    return <Main onButtonClick={handleLoginClick} />;
   } else {
     return (
       <div className="App">
