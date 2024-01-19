1f717aa55feb225de7eddb86ce3e098be9637c92
Kyprosa <propolis200@gmail.com>
2024-01-19 12:10:28 +0900

    online

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,44 +1,16 @@
-import { useState } from "react";
-import Modal from "react-modal";
+import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/gamePageStyle.css";
 
 function Game() {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const handleCreateRoom = () => {
-    console.log("버튼 클릭됨");
-  };
   return (
     <div className="body">
       <h2>Game</h2>
       <ul>
         <li>
-          <input
-            type="button"
-            id="onlineBtn"
-            onClick={() => setModalIsOpen(true)}
-          />
-          {/*방 만들기 모달창*/}
-          <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={() => setModalIsOpen(false)}
-            appElement={document.getElementById("root") || undefined}
-          >
-            <div id = "container">
-              <button
-                  id="modalBtn"
-                  onClick={handleCreateRoom}
-              >
-                Create Room
-              </button>
-              <input
-                  id = "myinput"
-                  value="방 이름 검색"
-              />
-              <button id="modalBtn"> Search </button>
-              <button onClick={() => setModalIsOpen(false)}>x</button>
-            </div>
-          </Modal>
+          <Link to="/game/online">
+            <div id="onlineBtn"></div>
+          </Link>
         </li>
         <li>
           <Link to="/game/offline">
