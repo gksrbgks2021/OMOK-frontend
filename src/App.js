9bea984f1a2a382697a578d1be0bfb4cfddb7dfa
yaho <rlatjwls2923@gmail.com>
2024-01-19 14:17:42 +0900

    upated prank, rankCss, app pages

--- src/App.js
+++ src/App.js
@@ -1,80 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import GomokuBoard from "./components/GomokuBoard.tsx";
-import Online from "./pages/online.js";
 import Main from "./pages/main.js";
 import Game from "./pages/game.js";
 import Tab from "./pages/friend.js";
 import Rank from "./pages/rank.js";
-//import Rank2 from "./pages/rank2";
 import Profile from "./pages/profile.js";
 import NavigationBar from "./NavigationBar.js";
 import StatusBar from "./StatusBar.js";
 import "./App.css";
-import { GOOGLE, KAKAO, GUEST } from "./constants/VARIABLE";
-import axios from "axios";
-import { URL_POST_TEST } from "./constants/UrlConstants";
 
 function App() {
-  const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수 얻기
-
-  const [showMain, setShowMain] = useState(() => {
-    // 로컬 스토리지에서 showMain 상태를 불러오기
-    const storedShowMain = localStorage.getItem("showMain");
-    return storedShowMain ? JSON.parse(storedShowMain) : true;
-  });
-
-  const handleLoginClick = (category) => {
-    switch (category) {
-      case GOOGLE:
-        handleGoogleLogin();
-        break;
-      case KAKAO:
-        break;
-      case GUEST:
-        handleGuestLogin();
-        break;
-    }
-    setShowMain(!showMain);
-  };
-
-  const handleGoogleLogin = () => {
-    const url = URL_POST_TEST;
-    console.log(URL_POST_TEST);
-    axios({
-      method: "post",
-      url: url,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        console.log(`응답: `, response);
-        let data = response.data;
-        if (response.data) {
-          console.log("data", data);
-        }
-      })
-      .catch((error) => {
-        console.error("Error during POST request:", error);
-      });
-  };
-  const handleGuestLogin = () => {
-    console.log("게스트 로그인 처리합니다");
-  };
-
+  const [showMain, setShowMain] = useState(true);
   const handleButtonClick = () => {
     setShowMain(!showMain);
-    navigate("/"); // 경로를 변경하고 싶은 경로로 지정
   };
 
-  useEffect(() => {
-    // showMain 상태가 변경될 때 로컬 스토리지에 저장
-    localStorage.setItem("showMain", JSON.stringify(showMain));
-  }, [showMain]);
-
   if (showMain) {
-    return <Main onButtonClick={handleLoginClick} />;
+    return <Main onButtonClick={handleButtonClick} />;
   } else {
     return (
       <div className="App">
@@ -88,7 +31,6 @@ function App() {
             <Route path="/profile" element={<Profile />} />
 
             <Route path="/game/offline" element={<GomokuBoard />} />
-            <Route path="/game/online" element={<Online />} />
             <Route path="/*" element={"Not Found"} />
           </Routes>
         </div>
--- src/pages/rank.js
+++ src/pages/rank.js
@@ -1,39 +1,41 @@
+
 import "../styles/rankStyle.css";
 import User from "../components/User";
 import React, { Component } from "react";
 import { useState } from "react";
 import { styled } from "styled-components";
 
+// const Place = styled.div`
+//   background-color: #f2cd6e;
+//   border-radius: 15% 15% 0 0;
+//   width: 60px;
+//   height: 160px;
+//   margin-left: 20px;
+// `;
+//
+// class UserRank extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+//
+//   render() {
+//     return <User />;
+//   }
+// }
 
-const Place = styled.div`
-  background-color: #f2cd6e;
-  border-radius: 15% 15% 0 0;
-  width: 60px;
-  height: 160px;
-  margin-left: 20px;
-`
-
-class UserRank extends Component{
-  constructor(props){
-    super(props);
-    this.state={};
-  }
-
-  render(){
-    return(
-      <User />
-    )
-  }
-}
 function Rank() {
   return (
     <div id="wrapper">
       <h2>Rank</h2>
       <div id="block">
-        <div id="second">
+        <div id="box">
           <User />
-          <h1>2</h1>
+          <div id="second">
+            <h1>2</h1>
+          </div>
         </div>
+
         <div id="first">
           <User />
           <h1>1</h1>
