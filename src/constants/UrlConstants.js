32dce3ed94f3381d68dc1ab6283446cb53f3e7ab
gksrbgks2021 <39733405+gksrbgks2021@users.noreply.github.com>
2024-01-26 15:28:07 +0900

    ## sync

--- src/constants/UrlConstants.js
+++ src/constants/UrlConstants.js
@@ -5,3 +5,5 @@ export const URL_POST_SOCIALLOGIN = backdomain + "/api/sign/auth";
 export const URL_POST_CREATEROOM = backdomain + "/api/";
 
 export const URL_POST_TEST = backdomain + "/api/sign/auth/temp";
+
+export const URL_GET_GETALLROOM = backdomain+ "/chat/rooms";
--- src/pages/game.js
+++ src/pages/game.js
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import {useRef,useEffect, useState} from "react";
 import Modal from "react-modal";
 import { Link } from "react-router-dom";
 import "../styles/gamePageStyle.css";
 import * as StompJs from "@stomp/stompjs";
+import axios from "axios";
+import {URL_GET_GETALLROOM} from "../constants/UrlConstants";
+
 function Game() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [content, setContent] = useState("");
+  const [chatRoomData, setChatRoomData] = useState([]);
   const client = useRef({});
 
+  const messageField = useRef(null);
+  const myChatRoom = null;
   const connect = () => {
+    /*client 객체를 만듭니다.*/
     client.current = new StompJs.Client({
       brokerURL: 'ws://localhost:8002/ws/websocket',
       connectHeaders: {
@@ -18,12 +25,19 @@ function Game() {
       debug: function (str) {
         console.log(str);
       },
+      /*연결 됐을때 실행할 함수*/
       onConnect : () => {
         subscribe();
       }
+      ,
+      onStompError : (frame) => {
+        console.log('Broker reported error: ' + frame.headers['message']);
+        console.log('Additional details: ' + frame.body);
+      }
     });
 
-    client.current.activate(); // 메소드 자체가 없습니다.
+    /*활성화 시켜준다. */
+    client.current.activate();
   }
 
   const subscribe = () => {
@@ -38,14 +52,40 @@ function Game() {
     return () => disConnect();
   }, []);
 
+  useEffect(() => {
+    if(modalIsOpen){
+      console.log("get all chatroom 요청");
+      /*전송 요청을 합니다.*/
+      axios({
+        method: "get",
+        url: URL_GET_GETALLROOM,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+          .then(response => {
+            console.log(`응답: `, response);
+            let data = response.data;
+            setChatRoomData(data);
+          })
+          .catch(error => {
+            console.error("Error during get request:", error);
+          });
+    }
+  }, [modalIsOpen]);
+
   const addContent = (message) => {
     setContent(content.concat(message));
   }
 
-  const handler = (message) => {
+  const sendHandler = () => {
     if (!client.current.connected)
       return;
 
+    let message = messageField.current.value;
+    console.log("메시지 전송 요청", message);
+    /*메시지를 보냅니다.*/
+    client.current.send();
     client.current.publish({
       destination: '/app/hello',
       body: JSON.stringify({
@@ -84,11 +124,19 @@ function Game() {
                 Create Room
               </button>
               <input
+                  ref = {messageField}
                   id = "myinput"
-                  value="방 이름 검색"
               />
-              <button id="modalBtn"> Search </button>
+              <button id="modalBtn" onClick={sendHandler}> 메시지 보내기 </button>
               <button onClick={() => setModalIsOpen(false)}>x</button>
+            </div>
+            <div id = "list_room">
+              {chatRoomData && Object.keys(chatRoomData).map(channel => (
+                    <div key={channel}>
+                      <p>Channel Name: {channel}</p>
+                      <p>{JSON.stringify(chatRoomData[channel])}</p>
+                    </div>
+                ))}
             </div>
           </Modal>
         </li>
