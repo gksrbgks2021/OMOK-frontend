2d63d7553cfce2c2e199deb7116678ea9e87c30f
Kyprosa <propolis200@gmail.com>
2024-02-23 14:12:03 +0900

    룸리스트

--- src/pages/roomlist.js
+++ src/pages/roomlist.js
@@ -1,9 +1,33 @@
 import NavigationBar from "../NavigationBar.js";
 import StatusBar from "../StatusBar.js";
 import Room from "../components/room.js";
+import { useEffect, useRef, useState } from "react";
 import "../styles/roomlistStyle.css";
 
 function Roomlist(props) {
+  const [chatRoomData, setChatRoomData] = useState([]);
+
+  useEffect(() => {
+    console.log("방찾기!");
+    /*전송 요청을 합니다.*/
+    axios({
+      method: "get",
+      url: URL_GET_GETALLROOM,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(`응답: `, response);
+        let data = response.data;
+        setChatRoomData(data.data);
+        colsole.log("데이터 = ", data.data);
+      })
+      .catch((error) => {
+        console.error("Error during get request:", error);
+      });
+  });
+
   return (
     <div>
       <StatusBar />
