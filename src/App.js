9039ed11d38d9ab7bacc60681785c3c2d081a737
gksrbgks2021 <39733405+gksrbgks2021@users.noreply.github.com>
2024-02-23 17:55:47 +0900

    ## update : 소셜로그인 토큰 발급 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Route, Router, Routes, useNavigate } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Route, Router, Routes, useLocation, useNavigate} from "react-router-dom";
 import GomokuBoard from "./components/GomokuBoard.js";
 import MainPage from "./pages/main.js";
 import Game from "./pages/game.js";
@@ -10,43 +10,72 @@ import Profile from "./pages/profile.js";
 import NavigationBar from "./NavigationBar.js";
 import StatusBar from "./StatusBar.js";
 import "./App.css";
-import { GOOGLE, KAKAO, GUEST } from "./constants/VARIABLE";
+import {GOOGLE, KAKAO, GUEST} from "./constants/VARIABLE";
 import axios from "axios";
-import { URL_POST_TEST, URL_POST_SOCIALLOGIN } from "./constants/UrlConstants";
-import { useDispatch, useSelector } from "react-redux";
+import {URL_POST_TEST, URL_POST_SOCIALLOGIN} from "./constants/UrlConstants";
+import {useDispatch, useSelector} from "react-redux";
+import {showMain} from './stores/MainReducer';
+import base64 from "base-64";
+import JwtLoginProcess from "./utils/JwtLoginProcess";
 
 function App() {
-  const dispatch = useDispatch();
-  const hideMain = useSelector((state) => state.main.isMainShown);
+    const dispatch = useDispatch();
+    const isMainHide = useSelector((state) => state.main.isMainShown);
+    const location = useLocation();
+    const navigate = useNavigate()
 
-  useEffect(() => {
-    console.log("변경됨..: ", hideMain);
-  }, [hideMain]);
-  return (
-    <div className="App">
-      <div className="screen">
-        {!hideMain ? (
-          <MainPage />
-        ) : (
-          <>
-            <StatusBar />
-            <NavigationBar />
-            <Routes>
-              <Route path="/game" element={<Game />} />
-              <Route path="/friend" element={<Tab />} />
-              <Route path="/rank" element={<Rank />} />
-              <Route path="/profile" element={<Profile />} />
+    const [token, setToken] = useState(null);
 
-              <Route path="/game/:gameType" element={<GomokuBoard />} />
-              <Route path="/mainroom" element={<Online />} />
+    const decodingInfo = (mytoken) => {
+        //jwt토큰 디코딩
+        let payload = mytoken.substring(mytoken.indexOf('.')+1,mytoken.lastIndexOf('.'));
+        let decodingInfo = base64.decode(payload);
+        let decodingInfoJson = JSON.parse(decodingInfo);
+        return decodingInfoJson;
+    }
+    useEffect(() => {
+        console.log("변경됨..: ", isMainHide);
+    }, [isMainHide]);
 
-              <Route path="/*" element={"Not Found"} />
-            </Routes>
-          </>
-        )}
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        const searchParams = new URLSearchParams(location.search);
+        const tokenParam = searchParams.get('token');
+        if (tokenParam) {
+            setToken(tokenParam);
+            console.log("서버로부터 redirect 받아옴...", tokenParam);
+            dispatch(showMain());
+            navigate("/mainroom");
+        }
+    }, [location]);
+
+    return (
+        <div className="App">
+            <div className="screen">
+                {!isMainHide ? (
+                    <>
+                        <MainPage/>
+                    </>
+                ) : (
+                    <>
+                        <StatusBar/>
+                        <NavigationBar/>
+                        {token && <JwtLoginProcess token={token} />}
+                        <Routes>
+                            <Route path="/game" element={<Game/>}/>
+                            <Route path="/friend" element={<Tab/>}/>
+                            <Route path="/rank" element={<Rank/>}/>
+                            <Route path="/profile" element={<Profile/>}/>
+
+                            <Route path="/game/:gameType" element={<GomokuBoard/>}/>
+                            <Route path="/mainroom" element={<Online/>}/>
+
+                            <Route path="/*" element={"Page Not Found"}/>
+                        </Routes>
+                    </>
+                )}
+            </div>
+        </div>
+    );
 }
 
 /*
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,6 @@
 import "../styles/mainPageStyle.css";
 import {GOOGLE, KAKAO, GUEST } from "../constants/VARIABLE";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {URL_POST_SOCIALLOGIN, URL_POST_TEST} from "../constants/UrlConstants";
 import axios from "axios";
@@ -11,6 +11,7 @@ function Main() {
     const [isShowMain, setShowMain] = useState(true);
     const navigate = useNavigate();
 
+    const url = new URL(window.location.href);
     const dispatch = useDispatch();
 
     const onButtonClick = (category) => {
@@ -32,24 +33,8 @@ function Main() {
         const url = URL_POST_TEST;
         const googleurl = URL_POST_SOCIALLOGIN + "/" + GOOGLE;
 
-        console.log(URL_POST_TEST);
-        axios({
-            method: "post",
-            url: url,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(response => {
-                console.log(`응답: `, response);
-                let data = response.data;
-                if(response.data){
-                    console.log('data',data);
-                }
-            })
-            .catch(error => {
-                console.error("Error during POST request:", error);
-            });
+        window.location.href = googleurl;
+
     };
     const handleGuestLogin = () => {
         console.log("게스트 로그인 처리합니다");
@@ -60,13 +45,16 @@ function Main() {
     }, []);
 
   return (
-    <div>
-      <div id="mainLogo" />
-      <div id="omok">O-MOK</div>
-      <button className="loginBtn" id={GOOGLE} onClick={() => onButtonClick(GOOGLE)} />
-      <button className="loginBtn" id={KAKAO} onClick={() => onButtonClick(KAKAO)} />
-      <button className="loginBtn" id={GUEST} onClick={() => onButtonClick(GUEST)} />
-    </div>
+      <div>
+          <div id="mainLogo"/>
+          <div id="omok">O-MOK</div>
+
+
+          <button className="loginBtn" id={GOOGLE} onClick={() => onButtonClick(GOOGLE)}/>
+          <button className="loginBtn" id={KAKAO} onClick={() => onButtonClick(KAKAO)}/>
+          <button className="loginBtn" id={GUEST} onClick={() => onButtonClick(GUEST)}/>
+
+      </div>
   );
 }
 
